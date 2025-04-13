00a338a4a5f91c801518a329e9de5a9a53de0645
ASUSQQ <188549698@qq.com>
2025-04-13 21:06:14 +0800

    添加文件上传功能
    - 在 application.yml 中配置 MinIO 相关信息
    - 新增 FileService 接口和 FileServiceImpl 实现类，实现文件上传逻辑
    - 在 FileUploadController 中集成文件上传功能
    - 新增 GlobalExceptionHandler 全局异常处理器- 添加 MinioConfiguration 和 MinioProperties 类，用于配置 MinIO客户端
    - 在 common模块中添加 minio 相关依赖

---
+++ common/src/main/java/com/itheima/lease/common/minio/MinioConfiguration.java
@@ -0,0 +1,25 @@
+package com.itheima.lease.common.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MinioProperties.class)
+//@ConfigurationPropertiesScan("com.itheima.lease.common.minio")
+public class MinioConfiguration {
+
+    @Autowired
+    private MinioProperties minioProperties;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .endpoint(minioProperties.getEndpoint())
+                .build();
+    }
+}
--- web/web-admin/src/main/java/com/itheima/lease/web/admin/service/impl/FileServiceImpl.java
+++ web/web-admin/src/main/java/com/itheima/lease/web/admin/service/impl/FileServiceImpl.java
@@ -1,9 +1,64 @@
 package com.itheima.lease.web.admin.service.impl;
 
+import com.itheima.lease.common.minio.MinioProperties;
 import com.itheima.lease.web.admin.service.FileService;
+import io.minio.*;
+import io.minio.errors.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class FileServiceImpl implements FileService {
 
+    @Autowired
+    private MinioClient minioClient;
+
+    @Autowired
+    private MinioProperties minioProperties;
+
+    @Override
+    public String upload(MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        // 检查存储桶是否存在，如果不存在则创建
+        boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(minioProperties.getBucketName()).build());
+        if (!bucketExists) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(minioProperties.getBucketName()).build());
+            minioClient.setBucketPolicy(SetBucketPolicyArgs.builder().bucket(minioProperties.getBucketName()).config(createBucketPolicyConfig(minioProperties.getBucketName())).build());
+        }
+
+        // 生成文件名
+        String filename = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+
+        // 上传文件到MinIO
+        minioClient.putObject(PutObjectArgs.builder().bucket(minioProperties.getBucketName()).stream(file.getInputStream(), file.getSize(), -1).object(filename).contentType(file.getContentType()).build());
+
+        return String.join("/", minioProperties.getEndpoint(), minioProperties.getBucketName(), filename); // 返回上传文件的路径
+    }
+
+    /**
+     * 创建存储桶策略配置
+     *
+     * @param bucketName 存储桶名称
+     * @return 存储桶策略配置字符串
+     */
+    private String createBucketPolicyConfig(String bucketName) {
+        return """
+                {
+                  "Statement" : [ {
+                    "Action" : "s3:GetObject",
+                    "Effect" : "Allow",
+                    "Principal" : "*",
+                    "Resource" : "arn:aws:s3:::%s/*"
+                  } ],
+                  "Version" : "2012-10-17"
+                }
+                """.formatted(bucketName);
+    }
 }
